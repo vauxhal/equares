754eaf23ddf2733434e8f744f09506b5caa1323e
Stepan Orlov <majorsteve@mail.ru>
2013-12-18 08:56:48 +0400

    Implemented escaping in string literals printed with the -d option.

--- equares_con/main.cpp
+++ equares_con/main.cpp
@@ -205,9 +205,35 @@ inline void printContainer(ostream& os, const Container& c, const Printer& p, co
     }
 }
 
-inline QString escapeString(const QString& s) {
-    // TODO
-    return s;
+inline QString escapeString(const QString& str)
+{
+    QByteArray resultu8;
+    foreach (char c, str.toUtf8())
+    {
+        switch (c)
+        {
+        case '\'':    resultu8 += "\\'";   break;
+        case '\\':   resultu8 += "\\\\";   break;
+        case '\a':    resultu8 += "\\a";   break;
+        case '\b':    resultu8 += "\\b";   break;
+        case '\f':    resultu8 += "\\f";   break;
+        case '\n':    resultu8 += "\\n";   break;
+        case '\r':    resultu8 += "\\r";   break;
+        case '\t':    resultu8 += "\\t";   break;
+        case '\v':    resultu8 += "\\v";   break;
+        default:
+            if (c >= 0   &&   c < 0x20)
+            {
+                resultu8 += "\\x";
+                if (c < 0x10)
+                    resultu8 += "0";
+                resultu8 += QString::number(c, 16);
+            }
+            else
+                resultu8 += c;
+        }
+    }
+    return QString::fromUtf8(resultu8);
 }
 
 template< class X >
