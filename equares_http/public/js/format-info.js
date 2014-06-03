7e3e457b69e45492e7ae0fe000cf7e37763b51cc
Stepan Orlov <majorsteve@mail.ru>
2014-06-03 14:30:25 +0400

    Implemented foreign link formatting in docs.

--- equares_http/public/js/format-info.js
+++ equares_http/public/js/format-info.js
@@ -1,5 +1,24 @@
 var formatInfo = {}
 formatInfo.init = function (elementId) {
+    var myren = new marked.Renderer
+    myren.link = function(href, title, text, external) {
+        var foreign = href.match('://') && !href.match('^' + location.origin)
+        if (arguments.length < 4)
+            external = foreign
+        var out = '<a href="' + href + '"'
+        if (title)
+          out += ' title="' + title + '"'
+        if (foreign)
+            out += ' class="external"'
+        if (external)
+            out += ' target="_blank"'
+        out += '>'
+        if (foreign)
+            out += '<img src="/images/xlink.png"/>'
+        out += text + '</a>';
+        return out;
+    }
+
     // Initialize MathJax
     MathJax.Hub.Config({
         elements: [elementId],
@@ -14,7 +33,7 @@ formatInfo.init = function (elementId) {
 
     MathJax.Hub.Register.MessageHook("End Process", function (message) {
         var e = $('#' + elementId)
-        e.html(marked(e.html()))
+        e.html(marked(e.html(), {renderer: myren}))
     })
 
     function LatexEquationNumbersReset() {
@@ -29,17 +48,15 @@ formatInfo.init = function (elementId) {
         if (m)
             for (var i=0; i<m.length; ++i) {
                 var v = m[i], mm = data.match(rx)
-                var a = mm[1] + '<a href="' + mm[4] + '"'
-                if (mm[2] === '?')
-                    a += ' target="_blank"'
-                a += '>'
-                data = data.replace(v, a + mm[3] + '</a>')
+                var a = myren.link(mm[4], null, mm[3], mm[2] === '?')
+                data = data.replace(v, mm[1] + a)
             }
         var e = $('#' + elementId)
         e.html(data)
         MathJax.Hub.Queue(LatexEquationNumbersReset, ["Typeset", MathJax.Hub, e[0]])
     }
+
     marked.setOptions({
         gfm: true
-    });
+    })
 }
