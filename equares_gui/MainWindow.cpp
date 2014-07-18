9f46e054e7e95583987c71176c133b62989a55cd
Stepan Orlov <majorsteve@mail.ru>
2014-07-18 08:21:18 +0400

    Modified license GPL v.3 to Affero GPL v.3. Added copyright notice and a reference to it from (almost) all source files.

--- equares_gui/MainWindow.cpp
+++ equares_gui/MainWindow.cpp
@@ -1,3 +1,13 @@
+/*
+EquaRes - a tool for numerical analysis of equations.
+Copyright (C) 2014 Stepan Orlov
+
+This file is part of EquaRes.
+
+Full text of copyright notice can be found in file copyright_notice.txt in the EquaRes root directory.
+License agreement can be found in file LICENSE.md in the EquaRes root directory.
+*/
+
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
